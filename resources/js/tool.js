0a45ec0178ecfcab6db91237389c4351da240a35
scramatte <scramatte@id.nixus.es>
2024-05-17 23:13:13 +0200

    cleanup code

--- resources/js/tool.js
+++ resources/js/tool.js
@@ -11,17 +11,13 @@ Nova.booting((app, store) => {
         },
         async mounted() {
             if (this._.type?.__file?.endsWith('MainHeader.vue')) {
-                const screen = this._.attrs[ 'data-screen' ]
-                
-                console.log('language-switcher:mounted',screen);
-
                 const languages = Nova.config('nova_language_switcher').languages;
                 let selected = Nova.config('nova_language_switcher').current_lang;
 
                 const container = document.createElement('div')
                 container.className = 'relative z-50';
 
-                const vnode = createVNode(LanguageSwitcher, { 
+                const vnode = createVNode(LanguageSwitcher, {
                     langs: languages,
                     selectedDisplay:languages[selected],
                     selected:selected
@@ -32,7 +28,7 @@ Nova.booting((app, store) => {
 
                 const element = this._.vnode.el.querySelector('header > div:last-child > div:last-child > div');
                 if (element) {
-                    element.insertAdjacentElement('beforebegin', container)               
+                    element.insertAdjacentElement('beforebegin', container)
                 }
             }
         }
