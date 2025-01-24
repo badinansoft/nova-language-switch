716551205045b0e800040dac6acabcddbdf4111a
Shahab Zebari <shahab.z1994@gmail.com>
2025-01-25 01:34:33 +0300

    add support laravel nova 5

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,9 +1,17 @@
-let mix = require('laravel-mix')
+const path = require('path');
+let mix = require('laravel-mix');
 
-require('./nova.mix')
+require('./nova.mix');
 
 mix
-  .setPublicPath('dist')
-  .js('resources/js/tool.js', 'js')
-  .vue({ version: 3 })
-  .nova('badinansoft/language-switch')
+    .setPublicPath('dist')
+    .js('resources/js/tool.js', 'js')
+    .vue({ version: 3 })
+    .webpackConfig({
+        resolve: {
+            alias: {
+                'laravel-nova-ui': path.resolve(__dirname, 'node_modules/laravel-nova-ui'),
+            },
+        },
+    })
+    .nova('badinansoft/language-switch');
